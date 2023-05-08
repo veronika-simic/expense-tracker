4e6d9ddf81f71d9216f715780f61d844a9a97780
veronika-simic <veronikasimic21@gmail.com>
2023-05-08 11:32:37 +0200

    hardcode date, amount and title for expense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {id: 1, title: "Dog food", amount: "$24.56", date: new Date(2023, 2, 3) },
+    {id: 2, title: "Cat food", amount: "$32.56", date: new Date(2023, 2, 3) },
+    {id: 3, title: "Human food", amount: "$124.56", date: new Date(2023, 2, 3) },
+  ];
   return (
-    <div className="App">
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+    <div>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,19 @@
-function ExpenseItem() {
-  return <h2>ExpenseItem</h2>;
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  return (
+    <div>
+      <div>{props.title}</div>
+
+      <h2>{props.amount}</h2>
+      <h2>{month}</h2>
+      <h2>{day}</h2>
+      <h2>{year}</h2>
+    </div>
+  );
 }
+
+export default ExpenseItem;
