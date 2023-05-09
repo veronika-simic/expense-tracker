a9a95502ec7b71e82fae697dba753048a7e275e7
veronika-simic <veronikasimic21@gmail.com>
2023-05-09 08:14:05 +0200

    update title, amount and date based on user input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,14 +1,60 @@
 import "./ExpenseForm.css";
-
+import { useState } from "react";
 function ExpenseForm() {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  function titleChangeHandler(event) {
+    setTitle(event.target.value);
+  }
+
+  function amountChangeHandler(event) {
+    setAmount(event.target.value);
+  }
+
+  function dateChangeHandler(event) {
+    setDate(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const expenseData = {
+      title,
+      amount,
+      date: new Date(date),
+    };
+    console.log(expenseData);
+    setTitle("");
+    setDate("");
+    setAmount("");
+  }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label>Title</label>
-      <input type="text" name="title"></input>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={titleChangeHandler}
+      ></input>
       <label>Amount</label>
-      <input type="number" name="amount" min="0.1" step="0.01"></input>
+      <input
+        type="number"
+        name="amount"
+        min="0.1"
+        step="0.01"
+        value={amount}
+        onChange={amountChangeHandler}
+      ></input>
       <label>Date</label>
-      <input type="date" name="date" min="2019-01-01" max="2024-01-01"></input>
+      <input
+        type="date"
+        name="date"
+        min="2019-01-01"
+        max="2024-01-01"
+        value={date}
+        onChange={dateChangeHandler}
+      ></input>
       <button type="submit">Add expense</button>
     </form>
   );
