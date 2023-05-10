c64e6d311df248727348ba5a3ec3f06d31889518
veronika-simic <veronikasimic21@gmail.com>
2023-05-10 10:46:29 +0200

    replace flex with grid

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,15 +1,14 @@
 import "./ExpenseDate.css";
-import Card from "../UI/Card";
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const year = props.date.getFullYear();
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   return (
-    <Card>
+    <>
       <h2 className="date">
         {day} {month} {year}
       </h2>
-    </Card>
+    </>
   );
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,14 @@
-import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
-import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
 function Expenses(props) {
   return (
-    <div className="container">
-      {props.items.map((item) => (
-        <ExpenseItem item={item} key={item.id} />
-      ))}
-    </div>
+    <>
+      <div className="container">
+        {props.items.map((item) => (
+          <ExpenseItem item={item} key={item.id} />
+        ))}
+      </div>
+    </>
   );
 }
 
