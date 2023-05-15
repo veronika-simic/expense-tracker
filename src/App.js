414a899f2de82db5aeb4b012f4651b2d136dd6e2
veronika-simic <veronikasimic21@gmail.com>
2023-05-15 12:23:00 +0200

    create button for adding new expense

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import "./App.css";
 import { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import ExpensesAmount from "./components/Expenses/ExpensesAmount";
 import ExpenseSearch from "./components/Expenses/ExpenseSearch";
 function App() {
@@ -40,19 +41,33 @@ function App() {
     },
   ];
   const [newExpenses, setNewExpenses] = useState(expenses);
+  const navigate = useNavigate();
   function addExpenseHandler(expense) {
     setNewExpenses((prevExpenses) => [expense, ...newExpenses]);
+  }
+  function navigateToNewExpense() {
+    navigate("/newexpense",);
   }
 
   return (
     <div>
-      <h1>
-        EXPENSE <br></br> TRACKER
-      </h1>
-      <ExpenseSearch></ExpenseSearch>
-      {/* <NewExpense onAddExpense={addExpenseHandler}></NewExpense> */}
-      <ExpensesAmount items={newExpenses} />
-      <Expenses items={newExpenses}></Expenses>
+      <div>
+        <h1>
+          EXPENSE <br></br> TRACKER
+        </h1>
+        <ExpenseSearch></ExpenseSearch>
+        <ExpensesAmount items={newExpenses} />
+        <Expenses items={newExpenses}></Expenses>
+      </div>
+      <button onClick={navigateToNewExpense}>+</button>
+      <Routes>
+
+      <Route
+          path="/newexpense"
+          element={<NewExpense onAddExpense={addExpenseHandler}></NewExpense>}
+        />
+      </Routes>
+      
     </div>
   );
 }
