a03cb552202df714f6851b700d9d8cf1401f4ff2
veronika-simic <veronikasimic21@gmail.com>
2023-05-16 06:36:31 +0200

    add login and sign up button

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -6,12 +6,16 @@ function Login() {
       <h2>Login</h2>
       <div>
         <label for="email">Email</label>
-        <input type="email" id="email"></input>
+        <input type="email" id="email" required></input>
       </div>
       <div>
         <label for="password">Password</label>
-        <input type="password" id="password"></input>
+        <input type="password" id="password" required></input>
       </div>
+      <button>Log in</button>
+      <h3>
+        New user? <a href="/">Sign up</a>
+      </h3>
     </form>
   );
 }
