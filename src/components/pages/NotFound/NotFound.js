89e30b32b515344f10b120dbec363ee9fe7842e0
veronika-simic <veronikasimic21@gmail.com>
2023-05-17 14:13:20 +0200

    add styling, positioning and image for 404 page

--- src/components/pages/NotFound/NotFound.js
+++ src/components/pages/NotFound/NotFound.js
@@ -1,8 +1,14 @@
+import "./NotFound.css";
+import { useNavigate } from "react-router-dom";
 function NotFound() {
+  const navigate = useNavigate();
+  function navigateToHomePage() {
+    navigate("/home");
+  }
   return (
-    <>
+    <div className="not-found-container">
       <div>
-        <image></image>
+        <img href="../../images/404.avif" alt="astronaut"></img>
       </div>
       <div>
         <h2>404</h2>
@@ -12,9 +18,9 @@ function NotFound() {
           mystery.
         </p>
         <p>But you can click the button below and go back to home page.</p>
-        <button>HOME</button>
+        <button onClick={navigateToHomePage}>HOME</button>
       </div>
-    </>
+    </div>
   );
 }
 
