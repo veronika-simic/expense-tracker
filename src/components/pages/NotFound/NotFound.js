d7648fa82fd229e7865ccb2a80cedd02b429ff6e
veronika-simic <veronikasimic21@gmail.com>
2023-05-17 10:31:50 +0200

    layout for 404 page

---
+++ src/components/pages/NotFound/NotFound.js
@@ -0,0 +1,21 @@
+function NotFound() {
+  return (
+    <>
+      <div>
+        <image></image>
+      </div>
+      <div>
+        <h2>404</h2>
+        <h3>UH OH!</h3>
+        <p>
+          The page you are looking for does not exist. How you got here is a
+          mystery.
+        </p>
+        <p>But you can click the button below and go back to home page.</p>
+        <button>HOME</button>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
