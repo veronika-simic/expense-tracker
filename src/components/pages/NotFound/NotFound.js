8792c6a982d1b6a57f8177c3ea83ff04b0a0ff94
veronika-simic <veronikasimic21@gmail.com>
2023-05-18 06:14:09 +0200

    change image for 404 page, add color, styling

--- src/components/pages/NotFound/NotFound.js
+++ src/components/pages/NotFound/NotFound.js
@@ -8,11 +8,10 @@ function NotFound() {
   return (
     <div className="not-found-container">
       <div>
-        <img href="../../images/404.avif" alt="astronaut"></img>
+        <img src={require("../../../images/404.png")} alt="astronaut"></img>
       </div>
       <div>
         <h2>404</h2>
-        <h3>UH OH!</h3>
         <p>
           The page you are looking for does not exist. How you got here is a
           mystery.
