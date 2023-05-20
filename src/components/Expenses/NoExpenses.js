e62a0df737276ee89dbcddd5248e67db1bea81c8
veronika-simic <veronikasimic21@gmail.com>
2023-05-20 09:49:21 +0200

    no expense info

---
+++ src/components/Expenses/NoExpenses.js
@@ -0,0 +1,20 @@
+import "./NoExpenses.css";
+import { useNavigate } from "react-router-dom";
+import NewExpense from "../NewExpense/NewExpense"
+function NoExpenses() {
+    const navigate = useNavigate()
+    function buttonClickHandler() {
+        navigate('/newexpense')
+    }
+  return (
+    <div className="no-expenses">
+      <h2>No expenses</h2>
+      <p>Either you are very frugal (not very likely)</p>
+      <p>or you are hiding something (most likely)</p>
+      <p>Quick add an expense to keep your reputation</p>
+      <button onClick={buttonClickHandler}>Add Expense</button>
+    </div>
+  );
+}
+
+export default NoExpenses;
