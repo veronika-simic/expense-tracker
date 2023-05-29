e4f058215b9f151d8e181f9530365c011e570039
veronika-simic <veronikasimic21@gmail.com>
2023-05-29 07:07:01 +0200

    fix conflict

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { ExpensesContextProvider } from "./context/ExpensesContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ExpensesContextProvider>
-      <ToastContainer position="bottom-left" theme="light" autoClose={2000} />
-      <App />
+      <AuthContextProvider>
+        <ToastContainer position="bottom-left" theme="light" autoClose={2000} />
+        <App />
+      </AuthContextProvider>
     </ExpensesContextProvider>
   </BrowserRouter>
 );
