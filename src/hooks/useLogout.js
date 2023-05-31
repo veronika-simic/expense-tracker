94fbad7f6fed3c946c6234180e27beb5eb842ea2
veronika-simic <veronikasimic21@gmail.com>
2023-05-31 07:46:49 +0200

    assign expenses to users

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,9 +1,12 @@
 import { useAuthContext } from "./useAuthContext";
+import { useExpensesContext } from "./useExpensesContext";
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: expensesDispatch } = useExpensesContext();
   const logout = () => {
     localStorage.removeItem("user");
     dispatch({ type: "LOGOUT" });
+    expensesDispatch({ type: "SET_EXPENSES", payload: null });
   };
   return { logout };
 };
